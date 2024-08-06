41f9a379b2e84bc95b5a5c569f9d391aa05217d4
A13D41B3N541D <abdalbensaid@gmail.com>
2024-08-06 16:37:17 +0000

    deux commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContratController;
+use App\Http\Controllers\SecteurController;
+use App\Http\Controllers\FinancesController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PersonnelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +18,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::prefix('admin/gestion/')->group(function () {
+    //routes pour le tableau de bord
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+
+    //routes pour les secteurs d'acivités 
+    Route::get('secteur', [SecteurController::class, 'index'])->name('secteur.index');
+
+    //routes pour les contrats
+    Route::get('contrat', [ContratController::class, 'index'])->name('contrat.index');
+
+    //routes pour la gestion de Personnels
+    Route::get('personnels', [PersonnelController::class, 'index'])->name('personnel.index');
+    
+    //routes pour la gestion de finances
+    Route::get('finances', [FinancesController::class, 'index'])->name('finance.index');
 });
